4dd5b4e5e90fce510e16483aa66a531a4990c28a
Ilyas <ilyasm.shahi359@gmail.com>
2023-03-26 05:01:25 +0500

    Nav Slider added

--- components/CategorySlider.jsx
+++ components/CategorySlider.jsx
@@ -1,32 +1,34 @@
-import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
-import 'swiper/css/navigation';
+import { useEffect, useRef, useState } from 'react';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 
 const CategorySlider = () => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  const swiperRef = useRef();
+
   return (
-    <>
+    <div className="md:px-6">
       <Swiper
         slidesPerView={3}
         spaceBetween={8}
         navigation={true}
-        modules={[Navigation]}
-        breakpoints={
-          // when window width is >= 320px
-          {
-            520: {
-              slidesPerView: 4,
-            },
-            670: {
-              slidesPerView: 6,
-            },
-            1020: {
-              slidesPerView: 8,
-            },
-          }
-        }
-        className="relative px-10 bg-red-100 mySwiper"
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        breakpoints={{
+          520: {
+            slidesPerView: 4,
+          },
+          670: {
+            slidesPerView: 6,
+          },
+          1020: {
+            slidesPerView: 8,
+          },
+        }}
+        className="relative !pr-24 mySwiper"
       >
         <SwiperSlide>
           <CategorySlide>Bestsellers</CategorySlide>
@@ -77,10 +79,35 @@ const CategorySlider = () => {
           <CategorySlide>Technology</CategorySlide>
         </SwiperSlide>
 
-        <div className="absolute top-0 left-0 z-10 w-60 h-10 bg-gradient-to-r from-white dark:from-slate-800" />
-        <div className="absolute top-0 right-0 z-10 w-60 h-10 bg-gradient-to-l from-white dark:from-slate-800" />
+        {activeSlideIndex > 0 && (
+          <button
+            className="absolute top-0 h-full left-0 z-20"
+            onClick={() => {
+              swiperRef?.current.slidePrev();
+              setActiveSlideIndex(swiperRef?.current?.activeIndex);
+            }}
+          >
+            <BsChevronLeft size={20} />
+          </button>
+        )}
+
+        <button
+          className="absolute top-0 h-full right-0 z-20"
+          onClick={() => {
+            swiperRef?.current.slideNext();
+            setActiveSlideIndex(swiperRef?.current?.activeIndex);
+          }}
+        >
+          <BsChevronRight size={20} />
+        </button>
+
+        {activeSlideIndex > 0 && (
+          <div className="absolute top-0 left-0 z-10 w-60 h-full bg-gradient-to-r from-white dark:from-slate-800" />
+        )}
+
+        <div className="absolute top-0 right-0 z-10 w-60 h-full bg-gradient-to-l from-white dark:from-slate-800" />
       </Swiper>
-    </>
+    </div>
   );
 };
 export default CategorySlider;
